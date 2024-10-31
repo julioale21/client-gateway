09ced4450544250ebb145f48cf310a2841b9a03c
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-31 09:29:35 -0300

    added orders ms comunication

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,28 @@
-export class CreateOrderDto {}
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalItems: number;
+
+  @IsEnum(OrderStatusList, {
+    message: `Order status must be one of ${OrderStatusList}`,
+  })
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
